66f16841cf9ec5b2b3b112b6c5a5d8e847ece784
turudek9991-sketch <turudek9991@gmail.com>
2026-07-01 19:26:27 +0700

    Update ProfileModel.java

--- app/src/main/java/com/dhub/ProfileModel.java
+++ app/src/main/java/com/dhub/ProfileModel.java
@@ -4,12 +4,12 @@ public class ProfileModel {
     public String packageName;
     public String cookie;
     public String link;
-    public String status; // idle | starting | running | error
+    public String status;
 
     public ProfileModel(String packageName) {
         this.packageName = packageName;
         this.cookie = "";
         this.link = "";
-        this.status = "idle";
+        this.status = "OFFLINE";
     }
 }
